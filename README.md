af4485afd030dbe8f1b9b7195c1d95711eb65697
Rahul De <rahul080327@gmail.com>
2018-12-31 15:45:44 +0100

    Simple working implementation

---
+++ README.md
@@ -0,0 +1,16 @@
+### Reference Bob Plugin: Git
+
+This is a simple plugin enabling Bob to read git repositories.
+
+#### Requirements
+- Python 3.5+
+- Git 1.7+
+
+#### Running
+- `pip3 install -r requirements.txt` to install dependencies.
+- `python3 server.py` will start the plugin on port 8000.
+
+#### API
+- `GET /bob_request`: Takes `repo` and `branch` as params, clones and
+   responds back with a zip of the repo.
+- `GET /ping`: Responds with an `Ack`.
---
+++ server.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+import tempfile
+
+from git import Repo
+from sanic import Sanic, response
+
+app = Sanic()
+
+
+@app.route("/bob_request")
+async def handle(request):
+    repo = request.args.get("repo")
+    branch = request.args.get("branch")
+
+    if any(map(lambda _: _ is None, [repo, branch])):
+        return response.text("Invalid params", status=400)
+
+    zip_file = "repo.zip"
+
+    if os.path.exists(zip_file):
+        os.remove(zip_file)
+
+    clone_dir = tempfile.mkdtemp()
+
+    Repo.clone_from(repo, clone_dir, branch=branch)
+
+    shutil.make_archive("repo", "zip", clone_dir)
+
+    shutil.rmtree(clone_dir)
+
+    return await response.file(zip_file)
+
+
+@app.route("/ping")
+async def handle_ping(_):
+    return response.text("Ack")
+
+
+if __name__ == "__main__":
+    app.run(port=8000)
